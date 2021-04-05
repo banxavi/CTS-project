917f4452673befc45257f021c32a5f6d22c562a6
Nhat <banxavi@gmail.com>
2021-04-05 10:45:51 +0700

    Nhat

--- MYSQL/cts_mission.sql
+++ MYSQL/cts_mission.sql
@@ -16,29 +16,33 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `schedule`
+-- Table structure for table `mission`
 --
 
-DROP TABLE IF EXISTS `schedule`;
+DROP TABLE IF EXISTS `mission`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `schedule` (
-  `Schedule_Id` int NOT NULL AUTO_INCREMENT,
-  `Mission_Id` int DEFAULT NULL,
-  `DateLoop` datetime DEFAULT NULL,
-  PRIMARY KEY (`Schedule_Id`),
-  KEY `FK_Mission_Id_idx` (`Mission_Id`),
-  CONSTRAINT `fk_mission_schedule` FOREIGN KEY (`Mission_Id`) REFERENCES `mission` (`Mission_Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `mission` (
+  `Mission_Id` int NOT NULL AUTO_INCREMENT,
+  `Title` varchar(255) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `StartDate` datetime DEFAULT NULL,
+  `EndDate` datetime DEFAULT NULL,
+  `State` int DEFAULT '1',
+  `Limit` int DEFAULT NULL,
+  `Point` int DEFAULT NULL,
+  PRIMARY KEY (`Mission_Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `schedule`
+-- Dumping data for table `mission`
 --
 
-LOCK TABLES `schedule` WRITE;
-/*!40000 ALTER TABLE `schedule` DISABLE KEYS */;
-/*!40000 ALTER TABLE `schedule` ENABLE KEYS */;
+LOCK TABLES `mission` WRITE;
+/*!40000 ALTER TABLE `mission` DISABLE KEYS */;
+INSERT INTO `mission` VALUES (1,'Đá Banh','Tham gia giải đá banh','2021-05-06 00:00:00','2021-06-07 00:00:00',1,50,100),(2,'Nhảy Dây','Tham gia khóa huấn luyện nhảy dây','2021-04-06 00:00:00','2022-05-04 00:00:00',1,100,100);
+/*!40000 ALTER TABLE `mission` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -50,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-31 11:37:03
+-- Dump completed on 2021-04-05 10:29:13
