2be2110bd3fe973e87f75b2f2d6b9171612acc2d
unknown <ducnguyenqn22@gmail.com>
2021-04-16 11:30:19 +0700

    Fixed conflict

--- SQL.py
+++ SQL.py
@@ -52,6 +52,7 @@ SQLOCKACC = 'UPDATE employee SET Status = %s WHERE Employee_Id = (%s)'
 SQLUNLOCKACC = 'UPDATE employee SET Status = %s WHERE Employee_Id = (%s)'
 # SHOW MISSION AVAIABLE
 SQLMISSION1 = 'select Mission_Id,Title,Description,StartDate,EndDate,State,`Limit`,Point,ROW_NUMBER() OVER(Order by mission.Mission_Id) as STT from mission where State=1'
+
 # SHOW MISSION OF USER
 SQLMISSIONUSER ='select   process.Process_Id, mission.Mission_Id, mission.Title \
                 ,mission.Description,mission.StartDate,mission.EndDate , mission.Point , \
@@ -59,6 +60,7 @@ SQLMISSIONUSER ='select   process.Process_Id, mission.Mission_Id, mission.Title 
                 where process.Employee_Id=employee.Employee_Id and \
                 process.Mission_Id=mission.Mission_Id \
                 and employee.Email = %s'
+SQLEXPORTEXCEL = "SELECT Employee_Id,Email,Name,Point,Status FROM employee"
 SQLCANCELMISSION = "UPDATE process set Status = 0 where Process_Id = %s"
 SQLUPDATELIMIT = "UPDATE mission inner join process\
                         on process.Mission_Id = mission.Mission_Id\
